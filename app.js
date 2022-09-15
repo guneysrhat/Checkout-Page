d92ee5c71ec5c792736e08ba8f7649e70c34186b
guneysrhat <guneysrhat@gmail.com>
2022-09-15 17:06:38 +0300

    Checkout_list

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const VintageBackbagPrice = 25.98;
 const LeviShoesPrice = 45.99;
 const AntiqueClockPrice = 74.98;
 let count1 = 1;
-
+let count2 = 1;
+let count3 = 1;
+let total1;
 
 //* Vintage Backbag
 
@@ -16,6 +18,7 @@ document.querySelector("#plus1").addEventListener("click", () => {
   document.querySelector("#total1").textContent = (
     VintageBackbagPrice * count1
   ).toFixed(2);
+
 });
 document.querySelector("#minus1").addEventListener("click", () => {
   if (count1 != 0) {
@@ -26,37 +29,57 @@ document.querySelector("#minus1").addEventListener("click", () => {
     ).toFixed(2);
   }
 });
+document.querySelector("#remove1").addEventListener("click", () => {
+  document.querySelector("#vintage").classList.add("d-none");
+  count1 = 0;
+});
+
 //* Levi Shoes
-document.querySelector("#plus1").addEventListener("click", () => {
-    count1++;
-    document.querySelector("#count1").textContent = count1;
-    document.querySelector("#total1").textContent = (
-      VintageBackbagPrice * count1
+
+document.querySelector("#plus2").addEventListener("click", () => {
+  count2++;
+  document.querySelector("#count2").textContent = count2;
+  document.querySelector("#total2").textContent = (
+    LeviShoesPrice * count2
+  ).toFixed(2);
+  
+});
+document.querySelector("#minus2").addEventListener("click", () => {
+  if (count2 != 0) {
+    count2--;
+    document.querySelector("#count2").textContent = count2;
+    document.querySelector("#total2").textContent = (
+      LeviShoesPrice * count2
     ).toFixed(2);
-  });
-  document.querySelector("#minus1").addEventListener("click", () => {
-    if (count1 != 0) {
-      count1--;
-      document.querySelector("#count1").textContent = count1;
-      document.querySelector("#total1").textContent = (
-        VintageBackbagPrice * count1
-      ).toFixed(2);
-    }
-  });
-  //*Antique Clock
-  document.querySelector("#plus1").addEventListener("click", () => {
-    count1++;
-    document.querySelector("#count1").textContent = count1;
-    document.querySelector("#total1").textContent = (
-      VintageBackbagPrice * count1
+  }
+});
+document.querySelector("#remove2").addEventListener("click", () => {
+  document.querySelector("#levi").classList.add("d-none");
+  count2 = 0;
+});
+
+//*Antique Clock
+
+document.querySelector("#plus3").addEventListener("click", () => {
+  count3++;
+  document.querySelector("#count3").textContent = count3;
+  document.querySelector("#total3").textContent = (
+    AntiqueClockPrice * count3
+  ).toFixed(2);
+  
+});
+document.querySelector("#minus3").addEventListener("click", () => {
+  if (count3 != 0) {
+    count3--;
+    document.querySelector("#count3").textContent = count3;
+    document.querySelector("#total3").textContent = (
+      AntiqueClockPrice * count3
     ).toFixed(2);
-  });
-  document.querySelector("#minus1").addEventListener("click", () => {
-    if (count1 != 0) {
-      count1--;
-      document.querySelector("#count1").textContent = count1;
-      document.querySelector("#total1").textContent = (
-        VintageBackbagPrice * count1
-      ).toFixed(2);
-    }
-  });
+  }
+});
+document.querySelector("#remove3").addEventListener("click", () => {
+  document.querySelector("#clock").classList.add("d-none");
+  count3 = 0;
+});
+
+
