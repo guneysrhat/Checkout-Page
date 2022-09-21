955f8be80d47a17bfa87034c8dbd82d9be0fa5c0
guneysrhat <guneysrhat@gmail.com>
2022-09-21 20:29:44 +0300

    storage

--- checkout.js
+++ checkout.js
@@ -1,1 +1,17 @@
+const TexRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
+
+window.addEventListener("load", () => {
+  //! set items to localStorage
+  localStorage.setItem("TexRate", TexRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+
+  //! set items to sessionStorage
+  sessionStorage.setItem("TexRate", TexRate);
+  sessionStorage.setItem("shippingPrice", shippingPrice);
+  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
+
 
