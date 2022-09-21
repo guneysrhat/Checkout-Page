b60107b05566bef6c55fa108cd02ac8dd750ed29
guneysrhat <guneysrhat@gmail.com>
2022-09-21 20:55:34 +0300

    quantity 0 remove

--- checkout.js
+++ checkout.js
@@ -14,4 +14,25 @@ window.addEventListener("load", () => {
   sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
-
+const productsDiv = document.querySelector(".products");
+//? Capturing vs. Bubbling
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    //console.log("minus btn is clicked!");
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Products will be removed?")) {
+        //*remove
+        event.target.parentElement.parentElement.parentElement.remove();
+      }
+    }
+  } else if (event.target.classList.contains("fa-plus")) {
+    //console.log("plus btn is clicked!");
+    event.target.previousElementSibling.innerText++;
+  } else if (event.target.className == "remove-product") {
+    //console.log("remove btn is clicked!");
+  } else {
+    //console.log("other element is clicked!");
+  }
+});
