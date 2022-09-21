cb860341e16009a8842841a9b10c007fe0f90ca8
guneysrhat <guneysrhat@gmail.com>
2022-09-21 22:01:28 +0300

    subtotal

--- checkout.js
+++ checkout.js
@@ -20,26 +20,56 @@ productsDiv.addEventListener("click", (event) => {
     //console.log("minus btn is clicked!");
     if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
       event.target.parentElement.querySelector(".quantity").innerText--;
+      calculateProductPrice(event.target);
+      calculateCartPrice();
     } else {
       if (confirm("Products will be removed?")) {
         //*remove
         event.target.parentElement.parentElement.parentElement.remove();
+        calculateCartPrice();
       }
     }
   } else if (event.target.classList.contains("fa-plus")) {
     //console.log("plus btn is clicked!");
     event.target.previousElementSibling.innerText++;
+    calculateProductPrice(event.target);
+    calculateCartPrice();
   } else if (event.target.className == "remove-product") {
     //console.log("remove btn is clicked!");
     event.target.parentElement.parentElement.parentElement.remove();
+    calculateCartPrice();
   } else {
     //console.log("other element is clicked!");
   }
 });
 
-const calculateProductPrice = () => {
-    
+const calculateProductPrice = (clickedBtn) => {
+  const productInfoDiv = clickedBtn.parentElement.parentElement;
+  // console.log(productInfoDiv);
+  const price = productInfoDiv.querySelector(".product-price strong").innerText;
+  const quantity = productInfoDiv.querySelector(".quantity").innerText;
+  const productTotalDiv = productInfoDiv.querySelector(".product-line-price");
+  productTotalDiv.innerText = (price * quantity).toFixed(2);
+  // alert(productTotalDiv);
 };
 const calculateCartPrice = () => {
+  const productTotalPricesDivs = document.querySelectorAll(
+    ".product-line-price"
+  );
+  //   foreach ==> NodeList, Array
+  //   const productTotalPricesDivs = [...document.getElementsByClassName(
+  //     ".product-line-price"
+  //   )];
 
+  let subtotal = 0;
+  productTotalPricesDivs.forEach((div) => {
+    subtotal += parseFloat(div.innerText);
+  });
+  console.log(subtotal);
+  const taxPrice = subtotal * localStorage.getItem("taxPate");
+  const shippingPrice =
+    subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice")
+      ? localStorage.getItem("shippingPrice")
+      : 0;
+  console.log(shippingPrice);
 };
